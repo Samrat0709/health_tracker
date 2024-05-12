c25be5f21bdd95f453d201abee6300f30af34123
Samrat0709 <118931358+Samrat0709@users.noreply.github.com>
2024-05-12 11:46:32 +0530

    Index page setup

--- index.php
+++ index.php
@@ -1,14 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <script src="script.js"></script>
-    <title>Health Tracker</title>
-</head>
+<?php include "./components/head.php" ?>
+<?php include "./components/script.php" ?>
+<?php include "./components/style.php" ?>
 <body>
-    <h1>This is the Landing Page of our website . Best of Luck Boys & Girl Lets Rock .... </h1>
+    <h1 class="text-danger">This is the Landing Page of our website . Best of Luck Boys & Girl Lets Rock .... </h1>
 
 </body>
 </html>
